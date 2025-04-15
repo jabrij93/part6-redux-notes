5c42286c53d7d3e17cd0ed7a5dbe0aa7f2e9952e
jabss <jabrijuhinin93@gmail.com>
2025-04-15 23:38:18 +0800

    expand noteReducer to include TOGGLE_IMPORTANCE

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,6 +1,19 @@
 export const noteReducer = (state = [], action) => {
-    if (action.type === 'NEW_NOTE') {
-      return state.concat(action.payload)
-    }  
-    return state
-}
+    switch(action.type) {
+      case 'NEW_NOTE':
+        return state.concat(action.payload)
+      case 'TOGGLE_IMPORTANCE': {
+        const id = action.payload.id
+        const noteToChange = state.find(n => n.id === id)
+        const changedNote = { 
+          ...noteToChange, 
+          important: !noteToChange.important 
+        }
+        return state.map(note =>
+          note.id !== id ? note : changedNote 
+        )
+       }
+      default:
+        return state
+    }
+  }
