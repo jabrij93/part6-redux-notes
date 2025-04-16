f596da32d07827a7af0ddc5d51125d0a94fd8632
jabss <jabrijuhinin93@gmail.com>
2025-04-16 14:29:14 +0800

    further refactor action creators from react components

--- src/main.jsx
+++ src/main.jsx
@@ -29,25 +29,33 @@ const generateId = () =>
   Number((Math.random() * 1000000).toFixed(0))
 
 const App = () => {
-  const addNote = (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    store.dispatch({
+  const createNote = (content) => {
+    return {
       type: 'NEW_NOTE',
       payload: {
         content,
         important: false,
         id: generateId()
       }
-    })
+    }
+  }
+
+  const toggleImportanceOf = (id) => {
+    return {
+      type: 'TOGGLE_IMPORTANCE',
+      payload: { id }
+    }
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    store.dispatch(createNote(content))
   }
 
   const toggleImportance = (id) => {
-    store.dispatch({
-      type: 'TOGGLE_IMPORTANCE',
-      payload: { id }
-    })
+    store.dispatch(toggleImportanceOf(id))
   }
 
   return (
