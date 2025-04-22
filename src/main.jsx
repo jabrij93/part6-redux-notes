99d160688ce58b8790bc7e8741e79c5a704f4914
jabss <jabrijuhinin93@gmail.com>
2025-04-22 21:00:15 +0800

    filter function is now working

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { Provider } from 'react-redux'
 import App from './App'
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
+import { createNote } from './reducers/noteReducer'
+import { filterChange } from './reducers/filterReducer'
 
 const reducer = combineReducers({
   notes: noteReducer,
@@ -18,16 +20,12 @@ const store = configureStore({
   reducer: reducer
 })
 
-console.log(store.getState())
+store.subscribe(() => console.log(store.getState()))
+store.dispatch(filterChange('IMPORTANT'))
+store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <div />
+    <App />
   </Provider>
 )
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>
-// )
