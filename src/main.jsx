f8143091d953c93bbb556dd081b88c6d09f84a70
jabss <jabrijuhinin93@gmail.com>
2025-04-22 20:25:54 +0800

    add filter reducer. and redux store state also changed though still rendering empty div

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 
 import App from './App'
 import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
+
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+
+// const store = configureStore(reducer2)
 
 const store = configureStore({
-  reducer: {
-    notes: noteReducer
-  }
+  reducer: reducer
 })
+
+console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <div />
   </Provider>
 )
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>
+// )
