0248c708e8d82ccae21bbd1408b8ac927dc6b10f
jabss <jabrijuhinin93@gmail.com>
2025-04-25 20:52:39 +0800

    cleanup of console.log

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ const store = configureStore({
   }
 })
 
-store.subscribe(() => console.log(store.getState()))
+// store.subscribe(() => console.log(store.getState()))
 store.dispatch(filterChange('IMPORTANT'))
 store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -1,5 +1,4 @@
 const filterReducer = (state = 'ALL', action) => {
-    console.log('ACTION: ', action)
 
     switch (action.type) {
         case 'SET_FILTER':
