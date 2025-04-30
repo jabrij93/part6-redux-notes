bd520645b1536ac2e6d654a77d9615e2394fa6a3
jabss <jabrijuhinin93@gmail.com>
2025-04-30 16:36:01 +0800

    cleanup and move redux store to store.js file

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
-
 import App from './App'
-import noteService from './services/notes'
-import noteReducer from './reducers/noteReducer'
-import filterReducer from './reducers/filterReducer'
-import { createNote } from './reducers/noteReducer'
-import { filterChange } from './reducers/filterReducer'
-
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer
-  }
-})
-
-// store.subscribe(() => console.log(store.getState()))
-// store.dispatch(filterChange('IMPORTANT'))
+import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
