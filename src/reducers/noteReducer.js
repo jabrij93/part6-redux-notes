169f147415fa3b4ad67d722ce460a2be7e8a472a
jabss <jabrijuhinin93@gmail.com>
2025-04-30 16:32:39 +0800

    update createNote function to it's own asynchronous action creator

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -35,12 +35,19 @@ const noteSlice = createSlice({
   },
 })
 
-export const { createNote, toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+export const { toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
 
 export const initializeNotes = () => {
   return async dispatch => {
     const notes = await noteService.getAll()
     dispatch(setNotes(notes))
+  }
+}
+
+export const createNote = content => {
+  return async dispatch => {
+    const newNote = await noteService.createNew(content)
+    dispatch(appendNote(newNote))
   }
 }
 
