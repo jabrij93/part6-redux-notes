ad04dd0d3460c9a9ab83cd34657271406cfeeeec
jabss <jabrijuhinin93@gmail.com>
2025-04-30 16:26:54 +0800

    separate initialization logic for the notes from react component

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice, current } from '@reduxjs/toolkit'
+import noteService from '../services/notes'
 
 const generateId = () =>
   Number((Math.random() * 1000000).toFixed(0)
@@ -35,6 +36,14 @@ const noteSlice = createSlice({
 })
 
 export const { createNote, toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+
+export const initializeNotes = () => {
+  return async dispatch => {
+    const notes = await noteService.getAll()
+    dispatch(setNotes(notes))
+  }
+}
+
 export default noteSlice.reducer
 
 // export default noteReducer
